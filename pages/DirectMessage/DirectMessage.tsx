ab1600c9523d0671a1954b795ffc8b04ba27d50d
Jinhyo <sosilion@gmail.com>
2021-03-17 20:41:44 +0900

    DireactMessage v1

--- pages/DirectMessage/DirectMessage.tsx
+++ pages/DirectMessage/DirectMessage.tsx
@@ -9,11 +9,56 @@ import { useParams } from "react-router";
 import useSWR, { useSWRInfinite } from "swr";
 import { Container } from "@pages/Channel/styles";
 import { Header } from "@pages/SignUp/styles";
+import ChatBox from "@components/ChatBox/ChatBox";
+import ChatList from "@components/ChatList/ChatList";
+import Scrollbars from "react-custom-scrollbars";
 
 const DireactMessage = () => {
   const { workspace, id } = useParams<{ workspace: string; id: string }>();
   const { data: userData } = useSWR(`/api/workspaces/${workspace}/users/${id}`, fetcher);
   const { data: myData } = useSWR("/api/users", fetcher);
+  const { data: chatData, mutate: mutateChat, revalidate, setSize } = useSWRInfinite<IDM[]>(
+    (index) => `/api/workspaces/${workspace}/dms/${id}/chats?perPage=20&page=${index + 1}`,
+    fetcher
+  );
+  console.log("🚀 ~ file: DirectMessage.tsx ~ line 21 ~ DireactMessage ~ chatData", chatData);
+
+  const [chat, onChangeChat, setChat] = useInput("");
+  // const scrollbarRef = useRef<Scrollbars>(null);
+
+  const onSubmitForm = useCallback(
+    (e) => {
+      e.preventDefault();
+      console.log(chat);
+      if (chat?.trim() && chatData) {
+        const savedChat = chat;
+        mutateChat((prevChatData) => {
+          prevChatData?.[0].unshift({
+            id: (chatData[0][0]?.id || 0) + 1,
+            content: savedChat,
+            SenderId: myData.id,
+            Sender: myData,
+            ReceiverId: userData.id,
+            Receiver: userData,
+            createdAt: new Date(),
+          });
+          return prevChatData;
+        }, false).then(() => {
+          setChat("");
+          // scrollbarRef.current?.scrollToBottom();
+        });
+        axios
+          .post(`/api/workspaces/${workspace}/dms/${id}/chats`, {
+            content: chat,
+          })
+          .then(() => {
+            revalidate();
+          })
+          .catch(console.error);
+      }
+    },
+    [chat, chatData, , myData, userData, workspace, id]
+  );
 
   if (!userData || !myData) {
     return null;
@@ -28,8 +73,13 @@ const DireactMessage = () => {
         />
         <span>{userData.nickname}</span>
       </Header>
-      {/* <ChatList chatSections={chatSections} scrollRef={scrollbarRef} setSize={setSize} isReachingEnd={isReachingEnd} />
-      <ChatBox chat={chat} onChangeChat={onChangeChat} onSubmitForm={onSubmitForm} /> */}
+      {/* <ChatList
+        chatSections={chatSections}
+        scrollRef={scrollbarRef}
+        setSize={setSize}
+        isReachingEnd={isReachingEnd}
+      /> */}
+      <ChatBox chat={chat} onChangeChat={onChangeChat} onSubmitForm={onSubmitForm} />
     </Container>
   );
 };
